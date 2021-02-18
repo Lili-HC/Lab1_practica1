da054428ea293f85e39814a49368903d1190e04f
Liliana <lilihuaranca13@gmail.com>
2021-02-17 20:39:51 -0400

    PROBLEMA 2 resuelto

--- problemas.js
+++ problemas.js
@@ -1,26 +1,43 @@
-//Resolviendo el Problema Nro 1
-let cade = prompt("Ingrese las cadenas que desee, como por ejemplo: aba aa ad vcd aba");
-document.write("<b>"+"Las cadenas con mayor tamaño son: "+"</b>");
-document.write("<p><b>"+ cadenasLargas(cade)+"</b></p>");
-function cadenasLargas(cad)
+//Resolviendo el Problema Nro 2
+let cade = prompt("Escriba una cadena");
+document.write("<b>"+"La nueva cadena es:"+"</b>");
+document.write("<p><b>"+ invertirparentesis(cade)+"</b></p>");
+function invertirparentesis(cadena) 
 {
-	let cadenas = cad.split(" ");
-	let tamaño=0;
-	let cadenas_largas=[];
-	for(let i=0;i<cadenas.length;i++)
-	{
-		if(cadenas[i].length>tamaño)
-		{
-			tamaño=cadenas[i].length;
-		}
-	}
-	for(cadena of cadenas)
-	{
-		if (cadena.length==tamaño) 
-		{
-			cadenas_largas.push(cadena);
-		}
-	}
-	console.log(cadenas_largas);
-	return cadenas_largas;
+    var cadenaFinal = "";
+    while(cadena.indexOf('(') > -1) 
+    {    
+        if(cadena.charAt(0) !== '(' && cadena.charAt(0) !== ')') 
+        {
+            cadenaFinal = cadenaFinal + cadena.charAt(0);
+            cadena = cadena.substring(0+1,cadena.length);
+        } 
+        else 
+        {    
+            if(cadena.charAt(0) === ')') 
+            {
+                cadena= cadena.substring(0+1,cadena.length);
+            } 
+            else 
+            {
+                var abriendoParenSiguiente = cadena.indexOf('(', 1);
+                var cerrandoParen = cadena.indexOf(')');
+                if(abriendoParenSiguiente !== -1 && abriendoParenSiguiente < cerrandoParen) 
+                {                    
+                    cadenaFinal = cadenaFinal + cadena.substring(abriendoParenSiguiente+1, cerrandoParen).split('').join('');
+                    var part_1 = cadena.slice(0,abriendoParenSiguiente);
+                    var part_2 = cadena.substring(cerrandoParen+1, cadena.length);
+                    cadena = part_1 + part_2;
+                } 
+                else 
+                {
+                    var cadenaInvertida = cadena.substring(1,cerrandoParen);
+                    cadenaFinal = cadenaFinal + cadenaInvertida.split('').reverse().join('');
+                    cadena = cadena.substring(cerrandoParen+1, cadena.length);
+                }
+            }  
+        }
+    }
+    cadenaFinal = cadenaFinal + cadena;
+    return cadenaFinal;
 }
